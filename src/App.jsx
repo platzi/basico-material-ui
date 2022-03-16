cf2655bd05f367e7d35b7070870758f0a7676b3d
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2022-03-16 15:50:13 -0500

    Added searcher component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+
+import Searcher from "./componets/Searcher";
 
 const App = () => {
+
+  const [inputUser, setInputUser] = useState('octocat');
+  const [userStater, userState] = useState('inputUser');
+
   return(
-    <div>Soy un platzinauta</div>
+    <Container sx={{
+      background: 'whitesmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }} >
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+    </Container>
   )
 };
 
